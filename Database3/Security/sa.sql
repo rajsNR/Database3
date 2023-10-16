dd2c8edce2d0a57942d6156736278fda4cd0efed
rajsNR <raj.s@neuralrays.ai>
2023-10-16 18:14:45 +0530

    Commented all local SQL related stuff

--- Database3/Security/sa.sql
+++ Database3/Security/sa.sql
@@ -1,7 +1,7 @@
-﻿CREATE LOGIN [sa]
-    WITH PASSWORD = N'NjkXk&mo:bgndbM`vw{{d4@HmsFT7_&#$!~<&jbayxvbsgkO', DEFAULT_LANGUAGE = [us_english];
+﻿--CREATE LOGIN [sa]
+--    WITH PASSWORD = N'NjkXk&mo:bgndbM`vw{{d4@HmsFT7_&#$!~<&jbayxvbsgkO', DEFAULT_LANGUAGE = [us_english];
 
 
-GO
-ALTER LOGIN [sa] DISABLE;
+--GO
+--ALTER LOGIN [sa] DISABLE;
 
